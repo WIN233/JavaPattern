304146e3d7687fecb2daeb724a37946bdaed05fb
WIN233 <1374862606@qq.com>
2019-09-27 14:00:01 +0800

    Initial commit

---
+++ src/main/java/com/qwk/pattern/simpleFactory/example1/Impl.java
@@ -0,0 +1,10 @@
+package com.qwk.pattern.simpleFactory.example1;
+/**
+ * ???????? 
+ */
+public class Impl implements Api{
+	
+	public void test1(String s) {
+		System.out.println("Now In Impl. The input s=="+s);
+	}
+}
---
+++ src/main/java/com/qwk/pattern/simpleFactory/example6/Impl2.java
@@ -0,0 +1,10 @@
+package com.qwk.pattern.simpleFactory.example6;
+/**
+ * 对接口的一种实现 
+ */
+public class Impl2 implements Api{
+	
+	public void test1(String s) {
+		System.out.println("Now In Impl2. The input s=="+s);
+	}
+}
